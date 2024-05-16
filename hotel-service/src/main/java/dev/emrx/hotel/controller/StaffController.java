c05031cf212c3b6f48012018f2b66f0de8e416c8
Elvis M <u201112959@upc.edu.pe>
2024-05-15 20:55:07 -0500

    Configuración centralizada con Spring Cloud Config Server

---
+++ hotel-service/src/main/java/dev/emrx/hotel/controller/StaffController.java
@@ -0,0 +1,23 @@
+package dev.emrx.hotel.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+
+    @GetMapping
+    public ResponseEntity<List<String>> listarStaffs() {
+        List<String> staffs = Arrays.asList("Christian", "Raul", "Biaggio", "Julen", "Dominik");
+
+        return ResponseEntity.ok(staffs);
+    }
+
+}
